e85bd669bacaf1a4f8b95d1a6b7b7c93c9737a78
Jeff Fong <jeff.fong@gmail.com>
2016-03-05 09:40:49 -0700

    test
    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')  // npm install express
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+
 //app.set('port', (process.env.PORT || 5000))  // uncomment me for local host and use port 5000
 app.use(express.static(__dirname + '/public'))
 
