5ee495079dade0922e681914fa005a1dc3b33ab9
Jeff Fong <jeff.fong@gmail.com>
2016-03-05 09:12:39 -0700

    commit server code
    added spinning server and conn to matlab

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')  // npm install express
 var app = express()
 
-//app.set('port', (process.env.PORT || 5000))
+//app.set('port', (process.env.PORT || 5000))  // uncomment me for local host and use port 5000
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
@@ -22,32 +22,20 @@ http.listen(process.env.PORT || 1337, function(){
   console.log('listening on *:' + process.env.PORT);
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('sendMsg', function(ret){
-  	console.log(ret);
-
-  	socket.emit('receiveMsg',"World");
-  });
-});
-
 
 
 var mongodb = require('mongodb'); // npm install mongodb 
 var MongoClient = mongodb.MongoClient;
-
 var uri = 'mongodb://xfyyc:xfyyc@ds037622.mongolab.com:37622/heroku_0dntxs3h';
 var collection;
 
-// Use connect method to connect to the Server
+
 MongoClient.connect(uri, function (err, db) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
     //HURRAY!! We are connected. :)
     console.log('Connection established to', uri);
-
-
     collection = db.collection('xfactoryyc1');
 
     var qry = { "Community": "Rideau Park" };
@@ -59,8 +47,74 @@ MongoClient.connect(uri, function (err, db) {
         }
       }
 
-    }); 
-
+    });    
 
   }
 });
+
+
+var Input;
+MongoClient.connect(uri, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    Input = db.collection('Input');
+  }
+});
+
+
+
+var mySocket;
+io.on('connection', function(socket){
+  console.log('a user connected');
+  mySocket = socket;
+  socket.on('sendMsg', function(ret){
+    console.log(ret);
+    socket.emit('receiveMsg',"World2");
+  });
+
+});
+
+//Input = db.collection('Input');
+setInterval(function() {
+
+  // find a question that is unanswered and return it to that user
+  console.log("a spin");
+  var qry = { "returned": false, "answered":true, "id":1 };
+  Input.find( qry ).toArray(function(err, docs) {  
+    
+    if (docs.length > 0) {
+      
+      console.log("found something");
+      for (j=0; j<docs.length; j++) {             
+        console.log(JSON.stringify(docs[j]));
+        
+        Input.update(qry, {$set: {returned:true}}, function (err, result) {
+          
+          if (err) {
+            console.log(err);
+          } else {
+
+            mySocket.emit('receiveMsg', "ans="+JSON.stringify(docs[j].answer));  //Server2Client
+            console.log("emitted to client");
+
+          } 
+
+        });
+
+        break;
+      }
+    }
+
+  }); 
+
+}, 2*1000);
+
+
+
+
+
+
+
+
